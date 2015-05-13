32be4a50dd7b60a331ffa8223d1e89e1b9bebac8
Alois Cochard <alois.cochard@gmail.com>
2015-05-13 14:52:47 +0200

    Enable `-Wall` GHC option and fix linting warnings.

--- src/System/Process/Machine.hs
+++ src/System/Process/Machine.hs
@@ -2,25 +2,24 @@
 module System.Process.Machine where
 
 import Data.Machine
-import Data.IOData (IOData, hGetLine, hPutStrLn)
+import Data.IOData (IOData)
 import System.Exit (ExitCode(..))
-import System.IO (Handle)
 import System.IO.Machine
-import System.Process (CreateProcess(..), ProcessHandle, StdStream(CreatePipe), createProcess, shell, waitForProcess)
+import System.Process (CreateProcess(..), ProcessHandle, createProcess, waitForProcess)
 
 type ProcessMachines a b k = (Maybe (ProcessT IO a b), Maybe (MachineT IO k a), Maybe (MachineT IO k a))
 
 mStdIn :: IOSource a -> ProcessMachines a a0 k0 -> IO ()
 mStdIn ms (Just stdIn, _, _)  = runT_ $ stdIn <~ ms
-mStdIn ms _                   = return ()
+mStdIn _  _                   = return ()
 
 mStdOut :: ProcessT IO a b -> ProcessMachines a a0 k0 -> IO [b]
 mStdOut mp (_, Just stdOut, _)  = runT $ mp <~ stdOut
-mStdOut mp _                    = return []
+mStdOut _  _                    = return []
 
 mStdErr :: ProcessT IO a b -> ProcessMachines a a0 k0 -> IO [b]
 mStdErr mp (_, _, Just stdErr)  = runT $ mp <~ stdErr
-mStdErr mp _                    = return []
+mStdErr _  _                    = return []
 
 callProcessMachines :: IOData a => forall b k. IODataMode a -> CreateProcess -> (ProcessMachines a b k -> IO c) -> IO (ExitCode, c)
 callProcessMachines m cp f = do
@@ -33,7 +32,7 @@ createProcessMachines :: IOData a => forall b k. IODataMode a -> CreateProcess -
 createProcessMachines (r, w) cp = do
   (pIn, pOut, pErr, pHandle) <- createProcess cp
   let pInSink = fmap (sinkHandleWith w) pIn
-  let pOutSource = fmap sourceHandle pOut
-  let pErrSource = fmap sourceHandle pOut
+  let pOutSource = fmap sourceHandle' pOut
+  let pErrSource = fmap sourceHandle' pErr
   return $ ((pInSink, pOutSource, pErrSource), pHandle) where
-    sourceHandle = sourceHandleWith r
+    sourceHandle' = sourceHandleWith r
